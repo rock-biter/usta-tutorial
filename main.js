909ac83116f3c83d59766b490fe07a8d49350584
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-04-29 11:42:04 +0200

    ADD: morphing starting point

--- main.js
+++ main.js
@@ -138,8 +138,8 @@ camera.lookAt(new THREE.Vector3(0, 2.5, 0))
 /**
  * Show the axes of coordinates system
  */
-const axesHelper = new THREE.AxesHelper(3)
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper(3)
+// scene.add(axesHelper)
 
 /**
  * renderer
